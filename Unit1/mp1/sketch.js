9c32c614c665dad35c5b4b11e7c67fe2ea4058a2
Disconnectors <disconnectorlol@gmail.com>
2022-01-27 11:46:48 -0600

    89

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  rectMode(CENTER);
+
 }
 
 function draw() {
+
+  if (mouseIsPressed) {
+    background('#2D936C');
+    // scene code
+
+  } else {
+    background("yellow");
+  }
+
+  rect(width / 2, height / 2, 200, 100);
 
 }
