145f202128b8417b5281aa291d5f4fae486c5ecc
Disconnectors <57683808+Disconnectors@users.noreply.github.com>
2022-03-01 23:43:39 -0600

    123

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -8,11 +8,14 @@ var ease = 0.1;
 let wineglass;
 let crackedwineglass;
 let breakingglass;
+let numberOfTouches;
+let char;
 
 function preload() {
   wineglass = loadImage('assets/wineglass.svg');
   crackedwineglass = loadImage('assets/crackedwineglass.svg');
   breakingglass = loadImage('assets/breakingglass.png');
+  char = loadImage('assets/char.png');
 }
 
 function setup() {
@@ -27,7 +30,7 @@ function draw() {
 
   vol = mic.getLevel();
   approachingVol += (vol - approachingVol) * ease;
-
+  numberOfTouches = touches.length;
   textSize(15);
   text("Have you ever wanted to break a wine glass with your singing?\n Well now you can!\n Click to start.\n My Volume is currently: " + vol.toFixed(3), width / 2, 20);
   z = int(map(approachingVol, 0, theLoudestItGets, 0, 3));
@@ -35,56 +38,59 @@ function draw() {
   switch (z) {
 
     case 0:
-      text("You're going to have to try harder than that...", width / 2, 475);
+      textSize(30);
+      text("SING!", width/2, 110);
       image(wineglass, width / 2 - 150, 125, 300, 300);
       break;
 
     case 1:
-      text("Almost...", width / 2, 475);
       translate(random(-5, 5), random(-5, 5));
       image(crackedwineglass, width / 2 - 150, 125, 300, 300);
       break;
     case 2:
-      text("Almost...", width / 2, 475);
       translate(random(-5, 5), random(-5, 5));
       image(crackedwineglass, width / 2 - 150, 125, 300, 300);
       break;
     case 3:
-      text("Almost...", width / 2, 475);
       translate(random(-5, 5), random(-5, 5));
       image(crackedwineglass, width / 2 - 150, 125, 300, 300);
       break;
     case 4:
-      text("Almost...", width / 2, 475);
       translate(random(-5, 5), random(-5, 5));
       image(crackedwineglass, width / 2 - 150, 125, 300, 300);
       break;
 
 
     case 5:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
     case 6:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
     case 7:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
     case 8:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
     case 9:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
     case 10:
-      text("Well done!", width / 2, 475);
       image(breakingglass, width / 2 - 150, 125, 300, 300);
       break;
+  }
+
+  switch (numberOfTouches) {
+    case 0:
+      textSize(15);
+      text("If more than one person is touching the screen, you get a surprise!\nCurrently: " + numberOfTouches + " people.", width/2, 475);
+    break;
+
+    case 1:
+      image(char, 0, 0, 500, 500);
+      text("Enjoy your charcuterie board!", width/2, 475);
+    break;
   }
 }
 
