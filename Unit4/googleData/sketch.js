4ce4c2621487bbd1a516d5c3aae1c7a6a6f51525
Disconnectors <disconnectorlol@gmail.com>
2022-04-07 11:44:26 -0500

    123

---
+++ Unit4/googleData/sketch.js
@@ -0,0 +1,72 @@
+var bubbles = [];
+let url = "";
+
+function setup() {
+  // let key = "1xG5lzBtJV3gK61ZE_qdku3ms9-pCJqwl0T8RVHI11m0"; // this is KEY of the URL from the sheet
+  let key = "1z3JlaLmqF0DavuQdzyhjllJ5NWwAQjwk5kmOs8vbh4o";
+  url = "https://opensheet.vercel.app/" + key + "/Form+Responses+1"; // here I'm making the string for loadJSON.
+
+  loadJSON(url, gotData);
+
+  // Regular setup code we usually have
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+}
+
+// The data comes back as an array of objects
+
+function gotData(data) {
+  console.log(data); // Print the data in the console
+
+  // add each line to an array of bubbles
+  for (let i = 0; i < data.length; i++) {
+    bubbles.push(
+      new Bubble(
+        data[i]["What is your favorite sport?"],
+        data[i]["What is your name?"],
+        data[i]["What is your major?"])); // THESE NEED TO MATCH SPREADSHEET
+
+  }
+}
+
+function draw() {
+  background("blue");
+
+  // // iterate through the bubbles and display the objects!
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].display();
+  }
+}
+
+// my Bubble class
+class Bubble {
+  constructor(sport, name, major) {
+    // only the order of these parameters matters!
+    this.sport = sport;
+    this.name = name;
+    this.major = major;
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(random(2, 5), 0);
+  }
+
+  display() {
+    stroke("red");
+    noFill();
+    ellipse(this.pos.x, this.pos.y+10, 120, 120);
+    fill("white");
+    text(
+      this.name + " likes \n" + this.sport + " and is in \n " + this.major,
+      this.pos.x,
+      this.pos.y
+    );
+
+    this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+
+
+  }
+
+
+}
